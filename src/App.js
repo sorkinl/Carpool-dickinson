4f17a2c4bc209dc606531efe0c36575ca357ee7e
Raph Lee <leer@dickinson.edu>
2020-05-07 12:41:56 -0400

    Button to load Search Results

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import SearchResults from './components/SearchResults'
+import { render } from '@testing-library/react';
 
-//test raph initial commit
+
+//temp function to load search Results
+function renderSearchResults(){
+  console.log("works")
+  render (
+    
+    <SearchResults />
+
+  );
+}
+
 function App() {
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
+      {/* button to load search results*/}
+      <button onClick={ 
+        renderSearchResults }>
+        search results button
+      </button>
+
     </div>
   );
 }
