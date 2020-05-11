3aada619268ba42a20b6943ae7bc5caea12abfd3
Raph Lee <leer@dickinson.edu>
2020-05-11 14:49:29 -0400

    d

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import SearchResults from './components/SearchResults'
-import { render } from '@testing-library/react';
+
 
 
 //temp function to load search Results
