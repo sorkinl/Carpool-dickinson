b7fb71e27054529eb1fb9af968e21b82f4250459
Raph Lee <leer@dickinson.edu>
2020-05-14 15:26:56 -0400

    Editted Each Result and added props

--- src/components/EachResult/EachResult.js
+++ src/components/EachResult/EachResult.js
@@ -1,41 +1,79 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import './EachResult.css';
-// import { Paper } from "@material-ui/core";
+
+import { Avatar} from "@material-ui/core";
+import Rating from '@material-ui/lab/Rating';
+import {List, ListItem, ListItemIcon, ListItemText } from "@material-ui/core";
+
 import star from "../../static/img/star.png"
 import avatar from "../../static/img/avatar.png"
+import PersonIcon from '@material-ui/icons/Person';
 
-function EachResult() {
+
+import LocationOnIcon from '@material-ui/icons/LocationOn';
+import LocationCityIcon from '@material-ui/icons/LocationCity';
+
+const useStyles = makeStyles((theme) => ({
+  ListItem:{
+    padding:0
+  },
+  ListItemIcon:{
+    minWidth: '40px'
+  },
+  avatarSize: {
+    width: theme.spacing(14),
+    height: theme.spacing(14),
+  }
+}));
+
+
+function EachResult(props) {
+
+  const classes = useStyles();
+
+  // var passedProps = {
+  //   name : this.props.name
+  // }
+
   return (
 
    
       <div className="eachResult">
-        
-        <img src = {avatar} alt = "temp"/>
 
 
+        <Avatar src = {avatar} alt = "temp" className = {classes.avatarSize}  />
+       
+        <List className = {classes.root}>
 
-        <div className = "eachResultQueryResult">
-  
-          <div className="top">
-            
-            <div className = "extraStatus">
-              SUPERHOST
-            </div>
-            <div className = "rating">
-              <img src = {star} alt = "ratingStar"/>
-              <span className ="ratingScore">4.5</span>     
-                (<span className ="ratingNumber">3</span>)           
-            </div>
+          {/* name */}
+          <ListItem button classes = {{root:classes.ListItem}} >
+            <ListItemIcon classes = {{root:classes.ListItemIcon}}>
+              <PersonIcon />
+            </ListItemIcon>
+            <ListItemText primary= {props.name} secondary={props.occupation}/>
+          </ListItem>
 
-          </div>
-        
-          <div>
-        b
-          </div>
-          <div className="bottom">
-              $24
-          </div>
-        </div>  
+          <ListItem button classes = {{root:classes.ListItem}} >
+            <ListItemIcon classes = {{root:classes.ListItemIcon}}>
+              <LocationOnIcon />
+            </ListItemIcon>
+            <ListItemText primary={props.starting} secondary={props.comment}/>
+          </ListItem>
+
+          <ListItem button classes = {{root:classes.ListItem}} >
+            <ListItemIcon classes = {{root:classes.ListItemIcon}}>
+              <LocationCityIcon/>
+            </ListItemIcon>
+            <ListItemText primary="Harrisburg" />
+          </ListItem>
+
+          <ListItem classes = {{root:classes.ListItem}} >
+            <Rating name="half-rating-read" defaultValue={props.rating} precision={0.5} readOnly />
+          </ListItem>
+        </List>
+
+    
       </div>
 
 
