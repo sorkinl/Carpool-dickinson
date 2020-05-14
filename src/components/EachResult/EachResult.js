a2803063c1c400283f613fba0e2533a90e3606a1
Raph Lee <leer@dickinson.edu>
2020-05-14 11:26:40 -0400

    Added avatar

--- src/components/EachResult/EachResult.js
+++ src/components/EachResult/EachResult.js
@@ -2,31 +2,40 @@ import React from 'react';
 import './EachResult.css';
 // import { Paper } from "@material-ui/core";
 import star from "../../static/img/star.png"
+import avatar from "../../static/img/avatar.png"
+
 function EachResult() {
   return (
 
    
       <div className="eachResult">
         
-        <div className="top">
-          
-          <div className = "extraStatus">
-            SUPERHOST
-          </div>
-          <div className = "rating">
-            <img src = {star} alt = "ratingStar"/>
-            <span className ="ratingScore">4.5</span>     
-              (<span className ="ratingNumber">3</span>)           
-          </div>
+        <img src = {avatar} alt = "temp"/>
 
-        </div>
-      
-        <div>
-      b
-        </div>
-        <div className="bottom">
-            $24
-        </div>
+
+
+        <div className = "eachResultQueryResult">
+  
+          <div className="top">
+            
+            <div className = "extraStatus">
+              SUPERHOST
+            </div>
+            <div className = "rating">
+              <img src = {star} alt = "ratingStar"/>
+              <span className ="ratingScore">4.5</span>     
+                (<span className ="ratingNumber">3</span>)           
+            </div>
+
+          </div>
+        
+          <div>
+        b
+          </div>
+          <div className="bottom">
+              $24
+          </div>
+        </div>  
       </div>
 
 
