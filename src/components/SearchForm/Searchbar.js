45dc974e6421c74732a3c7a0fe7683d3e05e84cf
jiyoon9607 <31414202+kimjiy@users.noreply.github.com>
2020-05-19 13:00:36 -0400

    Removed some imports

--- src/components/SearchForm/Searchbar.js
+++ src/components/SearchForm/Searchbar.js
@@ -2,16 +2,11 @@ import React, { Component, useState } from 'react';
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Input from '@material-ui/core/Input';
 import FormControl from '@material-ui/core/FormControl';
 import OutlinedInput from '@material-ui/core/OutlinedInput';
 import InputLabel from '@material-ui/core/InputLabel';
 
 import Link from '@material-ui/core/Link';
-import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
-import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 
