9308069e8001bd3e4bc3e620c9dc56bb153e7cbb
jiyoon9607 <31414202+kimjiy@users.noreply.github.com>
2020-05-28 13:46:56 -0400

    removed unused imports

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,6 +1,5 @@
 import { createStore, applyMiddleware } from "redux";
 import rootReducer from "../reducers/rootReducer";
-// import { userMiddleware } from "../middleware/usersMiddleware";
 import { authMiddleware } from "../middleware/authMiddleware";
 
 
