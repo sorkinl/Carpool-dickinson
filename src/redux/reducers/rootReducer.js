b685ead204b5b4f2d029d4ab76acfb5c295399b4
Katie Le <khanhlevany@gmail.com>
2020-05-30 15:01:10 +0700

    review for loggedIn prop undefined

--- src/redux/reducers/rootReducer.js
+++ src/redux/reducers/rootReducer.js
@@ -1,14 +1,11 @@
 import { combineReducers } from 'redux'
 import authReducer from './authReducer'
 import tripsReducer from './tripsReducer';
-// import { firestoreReducer } from 'redux-firestore';
-// import { firebaseReducer } from 'react-redux-firebase';
 
 const rootReducer =  combineReducers({
     authReducer,
-    tripsReducer
-    // firestoreReducer,
-    // firebaseReducer
+    tripsReducer,
+
 })
 
 export default rootReducer;
