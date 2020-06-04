bf255f1f4c0983e688f6ef7244dcf33bf478ddb6
Leonid Sorkin <lsorkini@gmail.com>
2020-06-04 20:21:02 +0300

    slight fix

--- src/redux/constants/auth-types.js
+++ src/redux/constants/auth-types.js
@@ -3,5 +3,6 @@ export const FOUND_BAD_WORD = "FOUND_BAD_WORD";
 export const REGISTER = "REGISTER";
 export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
 export const REGISTER_SUCCESS = "REGISTER_SUCCESS";
-export const SEND_EMAIL = "SEND_EMAIL"
+export const SEND_EMAIL = "SEND_EMAIL";
+export const LOGOUT_SUCCESS = "LOGOUT_SUCCESS"
 
