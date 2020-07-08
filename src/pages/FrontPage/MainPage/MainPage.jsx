58064e1dcfbeb3eb7865fb6ee4b178b2c4c691f6
Leonid Sorkin <lsorkini@gmail.com>
2020-07-08 17:21:13 +0300

    Leo did the header

---
+++ src/pages/FrontPage/MainPage/MainPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../Header/Header';
+
+const MainPage = () => {
+    return(
+        <>
+        <Header/>
+        </>
+    )
+}
+
+
+export default MainPage;
