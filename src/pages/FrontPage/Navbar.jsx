fa435033a61af28216576571d6e2f951adc2f547
Leonid Sorkin <lsorkini@gmail.com>
2020-07-11 20:08:56 +0300

    Somewhat navbar

--- src/pages/FrontPage/Navbar.jsx
+++ src/pages/FrontPage/Navbar.jsx
@@ -1,19 +1,42 @@
 import React from 'react';
+import {Link} from 'react-router-dom'
+import {useSelector, dispatch} from 'react-redux'
+import firebase from '../../firebase/firebaseConfig'
 const Navbar = () => {
+
+    const auth = useSelector((state) => state.firebase.auth);
+
+    function handleLogOut(e) {
+        e.preventDefault();
+        firebase.auth().signOut()
+      }
+
+    const links = auth.uid ? (
+        <>
+        <li className="navigation__item"><Link to="/" className="navigation__link" onClick={handleLogOut}>Log Out</Link></li>
+                    <li className="navigation__item"><a href="#" className="navigation__link">About</a></li>
+                    </>
+    ): (
+        <>
+        <li className="navigation__item"><Link to="/logIn" className="navigation__link">Login</Link></li>
+        <li className="navigation__item"><Link to="/signUp" className="navigation__link">Sign Up</Link></li>
+        <li className="navigation__item"><a href="#" className="navigation__link">About</a></li>
+        </>
+    )
     return(
     
         <div className="navigation">
             <input type="checkbox" className="navigation__checkbox" id="navi-toggle"/>
 
-            <label htmlFor="navi-toggle" className="navigation__button">Menu</label>
+            <label htmlFor="navi-toggle" className="navigation__button">
+                <span className="navigation__icon">&nbsp;</span>
+            </label>
 
             <div className="navigation__background">&nbsp;</div>
 
             <nav className="navigation__nav">
                 <ul className="navigation__list">
-                    <li className="navigation__item"><a href="#" className="navigation__link"></a>Login</li>
-                    <li className="navigation__item"><a href="#" className="navigation__link"></a>Sign Up</li>
-                    <li className="navigation__item"><a href="#" className="navigation__link"></a>About</li>
+                    {links}
                 </ul>
             </nav>
         </div>
