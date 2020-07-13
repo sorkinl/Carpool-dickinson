f9b65405a58db0e925d95dbcc31e2b2b86d95f4b
Katie Le <khanhlevany@gmail.com>
2020-07-13 17:17:32 +0700

    here you go postRideField + profile style in progress

--- src/components/Account/Account.jsx
+++ src/components/Account/Account.jsx
@@ -1,23 +1,49 @@
 import React from 'react';
-import './Account.scss'
 import Profile from './Profile/Profile';
 import TripList from './Trips/TripList';
-import {Grid, CssBaseline} from '@material-ui/core';
+import Record from './Record';
+import {
+  Grid, 
+  CssBaseline, 
+  Paper, 
+  Card,
+  CardHeader,
+  CardContent,
+  CardActions,
+  Typography,
+} from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 
-
+const useStyles = makeStyles((theme) => ({
+  container: {
+    marginTop: theme.spacing(5),
+    marginLeft: theme.spacing(3),
+    marginRight: theme.spacing(30),
+    padding: theme.spacing(2),
+  },
+}));
 export default function Account(props) {
+  const classes = useStyles();
     return(
         <CssBaseline>
-          <div className="account">
+           <header className="account-page">
             <Grid container spacing={1}>
-              <Grid item xs={4.5}>
+              <Grid item xs={4}>
                 <Profile/>
               </Grid>
+            
               <Grid item xs>
-                <TripList />
+                  <Grid container direction="column" spacing={2}>
+                      <Grid container spacing={1} className={classes.container}>
+                          <Record/>
+                      </Grid>
+                      <Grid item xs>
+                          <TripList />  
+                      </Grid>
+                  </Grid>    
               </Grid>
             </Grid>
-          </div>
+          </header>
         </CssBaseline>
       );
 }
--- src/components/Account/Profile/Profile.jsx
+++ src/components/Account/Profile/Profile.jsx
@@ -11,8 +11,6 @@ import {
     Grid,
     Collapse} from '@material-ui/core';
 import { useSelector } from 'react-redux';
-import './Profile.scss';
-
 const useStyles = makeStyles((theme)=>({
     root: {
         maxWidth: '100%',
@@ -43,41 +41,64 @@ export default function Profile(props){
 
     const classes = useStyles();
     const user = useSelector(state => state.firebase.profile);
-    return(
-        <div className={classes.root}>
-            {/* Display user's Name, school, and Email*/}
-            <Grid item>
-                <Card className={classes.root} id="card">
-                    <CardHeader
-                        title={
-                            <Typography className={classes.title} variant="h5">{user.firstName} {user.lastName}</Typography>
-                        }
-                        align='left'
-                        subheader={
-                            <>
-                                <Typography variant="subtitle1" color="textPrimary">{user.school}</Typography>
-                                <Typography variant="body2">{user.email}</Typography>
-                            </>
-                        }
-                        avatar={
-                            <img src={user.photoUrl}
-                                    alt="/broken-image.jpg"
-                                    className={classes.avatarSize}>
-                            </img>
-                        }
-                    />
-                    {/*Display the EditField if EditButton is clicked*/}
-                    <CardActions>
-                        <EditButton onClick={()=>{setClick(!isClicked)}} status={isClicked}/>
-                    </CardActions>
-                    {/* Expand Profile card and display EditField component*/}
-                    <Collapse in={isClicked} timeout="auto" unmountOnExit>
-                        <CardContent>
-                            <EditField user={user}/>
-                        </CardContent>
-                    </Collapse>
-                </Card>
-            </Grid>
+    // return(
+    //     <div className={classes.root}>
+    //         {/* Display user's Name, school, and Email*/}
+    //         <Grid item>
+    //             <Card className={classes.root} id="card">
+    //                 <CardHeader
+    //                     title={
+    //                         <Typography className={classes.title} variant="h5">{user.firstName} {user.lastName}</Typography>
+    //                     }
+    //                     align='left'
+    //                     subheader={
+    //                         <>
+    //                             <Typography variant="subtitle1" color="textPrimary">{user.school}</Typography>
+    //                             <Typography variant="body2">{user.email}</Typography>
+    //                         </>
+    //                     }
+    //                     avatar={
+    //                         <img src={user.photoUrl}
+    //                                 alt="/broken-image.jpg"
+    //                                 className={classes.avatarSize}>
+    //                         </img>
+    //                     }
+    //                 />
+    //                 {/*Display the EditField if EditButton is clicked*/}
+    //                 <CardActions>
+    //                     <EditButton onClick={()=>{setClick(!isClicked)}} status={isClicked}/>
+    //                 </CardActions>
+    //                 {/* Expand Profile card and display EditField component*/}
+    //                 <Collapse in={isClicked} timeout="auto" unmountOnExit>
+    //                     <CardContent>
+    //                         <EditField user={user}/>
+    //                     </CardContent>
+    //                 </Collapse>
+    //             </Card>
+    //         </Grid>
+    //     </div>
+    // );
+    return (
+        <div className="card outer-div">
+            <div className="card inner-div">
+                <div className="card front">
+                    <div className="card front__bkg-photo"></div>
+                    <img className ="card front__face-photo" 
+                        src={user.photoUrl}
+                        alt="/broken-image.jpg"/>
+                    
+                    <div className="card front__text">
+                        <h3 className="card front__text-header">{user.firstName} {user.lastName}</h3>
+                        <p className="card front__text-para">{user.school} | {user.classYear}</p>
+                    
+                        <p className="card front__text-sub"><i className="fa fa-envelope front-icons"></i>{user.email}</p>
+                        <p className="card front__text-sub-2"><i className="fa fa-phone front-icons"></i>{user.phone}</p>   
+                        <a href="#" className="btn btn--yell">
+                            Edit profile
+                        </a>
+                    </div>
+                </div>
+            </div>
         </div>
-    );
+    )
 }
