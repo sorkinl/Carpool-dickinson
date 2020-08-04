bc84d1f20d76f20f74151a50208b243803a58062
Leonid Sorkin <lsorkini@gmail.com>
2020-08-04 11:10:54 +0200

    style: chat

--- src/components/Chat/MainChat/MainChat.jsx
+++ src/components/Chat/MainChat/MainChat.jsx
@@ -10,6 +10,8 @@ import ListItemText from '@material-ui/core/ListItemText';
 import PersonInChat from '../PersonInChat/PersonInChat';
 import {useStyles} from './mainChatStyles';
 import DashboardNavbar from '../../Dashboard/DashboardNavbar';
+import {Link} from 'react-router-dom';
+import avatar from "../../../static/img/avatar.png"
 
 
 
@@ -18,22 +20,53 @@ const MainChat = () => {
   const classes = useStyles();
   
     return (
-      <>
+      <div className="container-dashboard">
       <DashboardNavbar/>
         <div className="chat-container">
           <div className="chat-sidebar">
-            Sidebar
+            
+            <Link >
+              <button className="chat-sidebar__link">
+              Return to Dashboard
+              </button>
+            </Link>
+            <ul className="chat-sidebar__list">
+                <Link>
+                <li className="chat-sidebar__list-element">Dixie Normus</li>
+                </Link>
+                <Link>
+                <li className="chat-sidebar__list-element">Jenny Talia</li>
+                </Link>
+                <Link>
+                <li className="chat-sidebar__list-element">Leo</li>
+                </Link>
+                <Link>
+                <li className="chat-sidebar__list-element">Yuri Tarted</li>
+                </Link>
+            </ul>
           </div>
           <div className="chat-window">
-            Main window
+            <div className="chat-window__top">
+
+              <p className="chat-window__message--out"><span className="chat-window__message">Hi</span></p>
+              <p className="chat-window__message--in"><span className="chat-window__message">How are you?</span> </p>
+              
+            </div>
+            <div className="chat-window__bottom">
+                <input type="text" className="chat-window__bottom--input"/>
+                <button className="chat-window__bottom--button">Send</button>
+            </div>
           </div>
 
         
         <div className="chat-rightbar">
-          Right bar
+          <img src={avatar} className="chat-rightbar__image">
+          </img>
+          <div className="chat-rightbar__name">Leo Sorkin</div>
+          <div className="chat-rightbar__major">Computer Science  &middot; Class 2022</div>
         </div>
         </div>
-        </>
+        </div>
         )
 }
 
